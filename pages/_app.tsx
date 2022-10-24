2a74489debcff34c8d839ee13b69bc3cebdcf9c3
highjoon <yundosa2@gmail.com>
2022-10-24 22:53:38 +0900

    [SETUP] Initial Setup

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
