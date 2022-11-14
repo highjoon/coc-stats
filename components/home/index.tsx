8707d6928a7c762167f990de4bc886b38bb38106
highjoon <yundosa2@gmail.com>
2022-11-14 15:30:47 +0900

    [DESIGN] 홈 배경화면 padding 무시, 404 페이지

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -4,7 +4,10 @@ import SearchBox from "components/search/searchBox";
 
 function HomeView() {
   return (
-    <main className="flex flex-col items-center justify-center w-full gap-10 py-20 m-auto bg-center bg-no-repeat bg-cover bg-homeBackground">
+    <main
+      id="layout"
+      className="flex flex-col items-center justify-center w-full gap-10 py-20 m-auto bg-center bg-no-repeat bg-cover bg-homeBackground"
+    >
       <Image
         src="/assets/images/logo/titleLogo.webp"
         alt="Title Logo"
