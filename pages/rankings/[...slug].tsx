9b0e12029edd892dc53735e14da8423620788efa
highjoon <yundosa2@gmail.com>
2022-11-22 02:45:56 +0900

    [FEATURE] SEO OG태그 설정

--- pages/rankings/[...slug].tsx
+++ pages/rankings/[...slug].tsx
@@ -19,6 +19,10 @@ function RankingsPage() {
       <Head>
         <title>Clash of Clans Stats - Rankings</title>
         <meta name="description" content="Clash of Clans Stats - 랭킹" />
+        <meta
+          name="og:description"
+          content="전 세계의 랭킹이 궁금해? 여기서 확인해봐!"
+        />
       </Head>
       <DynamicRankingsView />
     </>
